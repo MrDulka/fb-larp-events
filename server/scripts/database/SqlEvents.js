8a254d79e4fc8c139c23f8a09dd0522d49880abd
Tomas Dulka <dulajz@gmail.com>
2017-09-18 18:52:50 +0200

    Sqlevents should store all values that we know for event

--- server/scripts/database/SqlEvents.js
+++ server/scripts/database/SqlEvents.js
@@ -30,12 +30,12 @@ class SqlEvents extends Events {
         const web = event.web;
         let selectSql = `SELECT * FROM public.event WHERE web = '${web}'`;
 
-        let insertSql = `INSERT INTO public.event 
-            (name, description, loc, source, "from", "to", latitude, longitude, web, added_by) 
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+        let insertSql = `INSERT INTO public.event
+            (name, description, loc, source, "from", "to", latitude, longitude, web, added_by, amoutOfPlayers, language)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
         `;
         let values = [event.name, event.description, event.location.name, event.source, event.date.start_date, event.date.end_date,
-            event.location.latitude, event.location.longitude, web, 1];
+            event.location.latitude, event.location.longitude, web, 1, event.amountOfPlayers, event.language];
 
         this._pgPool.query(selectSql).then(result => {
             if (result.rows.length > 0) {
@@ -67,8 +67,6 @@ class SqlEvents extends Events {
      */
     convert(events) {
         return events.rows.map(event => {
-            let name = event.name;
-            let description = event.description;
             let date = {
                 start_date: new Date(event.from.toISOString()),
                 end_date: new Date(event.to.toISOString())
@@ -78,7 +76,7 @@ class SqlEvents extends Events {
                 longitude: event.longitude,
                 name: event.loc
             };
-            return new Event(name, description, date, location, event.web, 'LarpDb');
+            return new Event(event.name, event.description, date, location, event.web, 'LarpDb', amountOfPlayers, language);
         });
     }
 }
