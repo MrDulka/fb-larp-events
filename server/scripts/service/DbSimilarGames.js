9ae0e2b947626842efd8c001b8d2227f12a467b8
Jakub Balhar <jakub@balhar.net>
2017-09-29 12:16:49 +0200

    GEt better information about success.

--- server/scripts/service/DbSimilarGames.js
+++ server/scripts/service/DbSimilarGames.js
@@ -16,17 +16,21 @@ class DbSimilarGames{
      *  load games, calculates similar games and stores them in the database
      */
     load(){
+        this._logger.info(`DbSimilarGames#load`);
+
         this._sqlGames.load().then(games => {
             let similarGames = new SimilarGames(games, this._logger);
             this.save(similarGames.compareAllToAll());
         })
         .catch(err => {
-            this._logger(`DbSimilarGames#load Error:`, err);
+            this._logger.error(`DbSimilarGames#load Error:`, err);
         });
 
     }
 
     save(games){
+        this._logger.info(`DbSimilarGames#save`);
+
         games.forEach(game => {
             this._sqlGames.save(game);
         });
